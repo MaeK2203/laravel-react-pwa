f2dc71006727c1fb2f5f621632e848f20dfcb328
MaeK2203 <g.guzas.r@gmail.com>
2024-10-04 15:43:25 +0900

    pwa test

--- server/resources/ts/index.tsx
+++ server/resources/ts/index.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import React from 'react';
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register';
 
 createRoot(document.getElementById('app') as HTMLElement).render(<App />);
+
+registerSW();
